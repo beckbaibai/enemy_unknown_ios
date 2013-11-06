d4d7578a85e6788b46411530b27fac1d01946952
Beck Chen <beck@student-116-wless194-044.rice.edu>
2013-11-06 16:50:30 -0600

    Added support for Javascript to call Objective-C methods; introduced SBJson library.

--- Enemy Unknown/Controllers/GameWebViewController.m
+++ Enemy Unknown/Controllers/GameWebViewController.m
@@ -8,8 +8,11 @@
 
 #import "GameWebViewController.h"
 #import "UIWebView+EUAdditions.h"
+#import "SBJson.h"
 
 @interface GameWebViewController () <UIWebViewDelegate>
+
+@property (strong, nonatomic) SBJsonParser *json;
 
 @end
 
@@ -18,16 +21,63 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	NSString *fullURL = @"http://enemyunknown.nodejitsu.com";
+    
+	//NSString *fullURL = @"http://enemyunknown.nodejitsu.com";
+    NSString *fullURL = @"http://10.118.194.97:4004";
     NSURL *url = [NSURL URLWithString:fullURL];
     NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:requestObj];
+    
+    self.json = [[SBJsonParser alloc] init];
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
-    // Test
-    NSLog([self.webView title]);
+    // Choose "slayer" scenario
+    [self.webView stringByEvaluatingJavaScriptFromString:@"mobileStartGame(\"slayer\");"];
+}
+
+// Provide a way for Javascript code to call Objective-C method.
+// This dirty hack comes from:
+//     blog.techno-barje.fr//post/2010/10/06/UIWebView-secrets-part3-How-to-properly-call-ObjectiveC-from-Javascript/
+
+- (BOOL)webView:(UIWebView *)webView
+    shouldStartLoadWithRequest:(NSURLRequest *)request
+    navigationType:(UIWebViewNavigationType)navigationType
+{
+    NSString *requestString = [[request URL] absoluteString];
+    
+    if ([requestString hasPrefix:@"js-frame"]) {
+        NSArray *components = [requestString componentsSeparatedByString:@":"];
+        NSString *function = (NSString *)[components objectAtIndex:1];
+        NSString *argsAsString = [(NSString *)[components objectAtIndex:2] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        NSArray *args = (NSArray *)[self.json objectWithString:argsAsString];
+        
+        [self handleCall:function args:args];
+        
+        return NO;
+    }
+    
+    return YES;
+}
+
+// Handle function calls with the provided name and arguments.
+
+- (void)handleCall:(NSString *)functionName args:(NSArray *)args
+{
+    if ([functionName isEqualToString:@"endGame"]) {
+        
+        if ([args count] != 1)
+            NSLog(@"endGame takes exactly 1 argument!");
+        NSLog(@"endGame called successfully!");
+        
+    } else if ([functionName isEqualToString:@"alert"]) {
+        
+        NSLog(@"WENGWENGWENG!!");
+        
+    } else {
+        NSLog(@"Unimplemented method '%@'", functionName);
+    }
 }
 
 - (void)didReceiveMemoryWarning
