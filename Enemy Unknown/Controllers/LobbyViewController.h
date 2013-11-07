eb3c17da15ad7670dc7e51653bdbe668cf6920a2
Frank Zhang <Frank@student-106-wless94-028.rice.edu>
2013-11-07 16:34:28 -0600

    iAd added.

---
+++ Enemy Unknown/Controllers/LobbyViewController.h
@@ -0,0 +1,15 @@
+//
+//  LobbyViewController.h
+//  Enemy Unknown
+//
+//  Created by Frank Zhang on 11/7/13.
+//  Copyright (c) 2013 Comp 446. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
+
+@interface LobbyViewController : UIViewController
+@property (strong, nonatomic) IBOutlet ADBannerView *adView;
+
+@end
---
+++ Enemy Unknown/Controllers/LobbyViewController.m
@@ -0,0 +1,18 @@
+//
+//  LobbyViewController.m
+//  Enemy Unknown
+//
+//  Created by Frank Zhang on 11/7/13.
+//  Copyright (c) 2013 Comp 446. All rights reserved.
+//
+
+#import "LobbyViewController.h"
+
+@interface LobbyViewController ()
+
+@end
+
+@implementation LobbyViewController
+
+
+@end
