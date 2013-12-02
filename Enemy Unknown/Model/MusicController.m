f63e52a8b5d066f66475822cd7c17d4344800aaf
Frank Zhang <Frank@Franks-MacBook-Pro.local>
2013-12-01 23:10:09 -0600

    Sound Changes

--- Enemy Unknown/Model/MusicController.m
+++ Enemy Unknown/Model/MusicController.m
@@ -23,5 +23,78 @@
     }
 }
 
+-(void)initInGameSound{
+    NSString *soundFilePath1 = [[NSBundle mainBundle] pathForResource:@"attack_1" ofType:@"mp3"];
+    NSData *sampleData1 = [[NSData alloc] initWithContentsOfFile:soundFilePath1];
+    NSError *audioError1 = nil;
+    AVAudioPlayer *attack_1player = [[AVAudioPlayer alloc] initWithData:sampleData1 error:&audioError1];
+    if(audioError1 != nil) {
+        NSLog(@"An audio error occurred: \"%@\"", audioError1);
+    }
+    else {
+        [attack_1player setNumberOfLoops: -1];
+    }
+    NSString *soundFilePath2 = [[NSBundle mainBundle] pathForResource:@"attack_1" ofType:@"mp3"];
+    NSData *sampleData2 = [[NSData alloc] initWithContentsOfFile:soundFilePath2];
+    NSError *audioError2 = nil;
+    AVAudioPlayer *attack_2player = [[AVAudioPlayer alloc] initWithData:sampleData2 error:&audioError2];
+    if(audioError2 != nil) {
+        NSLog(@"An audio error occurred: \"%@\"", audioError2);
+    }
+    else {
+        [attack_2player setNumberOfLoops: -1];
+    }
+    NSString *soundFilePath3 = [[NSBundle mainBundle] pathForResource:@"attack1" ofType:@"mp3"];
+    NSData *sampleData3 = [[NSData alloc] initWithContentsOfFile:soundFilePath3];
+    NSError *audioError3 = nil;
+    AVAudioPlayer *attack1player = [[AVAudioPlayer alloc] initWithData:sampleData3 error:&audioError3];
+    if(audioError3 != nil) {
+        NSLog(@"An audio error occurred: \"%@\"", audioError3);
+    }
+    else {
+        [attack1player setNumberOfLoops: -1];
+    }
+    NSString *soundFilePath4 = [[NSBundle mainBundle] pathForResource:@"attack1" ofType:@"mp3"];
+    NSData *sampleData4 = [[NSData alloc] initWithContentsOfFile:soundFilePath4];
+    NSError *audioError4 = nil;
+    AVAudioPlayer *attack2player = [[AVAudioPlayer alloc] initWithData:sampleData4 error:&audioError4];
+    if(audioError4 != nil) {
+        NSLog(@"An audio error occurred: \"%@\"", audioError3);
+    }
+    else {
+        [attack2player setNumberOfLoops: -1];
+    }
+    NSString *soundFilePath5 = [[NSBundle mainBundle] pathForResource:@"attack1" ofType:@"mp3"];
+    NSData *sampleData5 = [[NSData alloc] initWithContentsOfFile:soundFilePath5];
+    NSError *audioError5 = nil;
+    AVAudioPlayer *backgroundplayer = [[AVAudioPlayer alloc] initWithData:sampleData5 error:&audioError5];
+    if(audioError5 != nil) {
+        NSLog(@"An audio error occurred: \"%@\"", audioError3);
+    }
+    else {
+        [backgroundplayer setNumberOfLoops: -1];
+    }
+    NSString *soundFilePath6 = [[NSBundle mainBundle] pathForResource:@"attack1" ofType:@"mp3"];
+    NSData *sampleData6 = [[NSData alloc] initWithContentsOfFile:soundFilePath6];
+    NSError *audioError6 = nil;
+    AVAudioPlayer *dieplayer = [[AVAudioPlayer alloc] initWithData:sampleData6 error:&audioError6];
+    if(audioError6 != nil) {
+        NSLog(@"An audio error occurred: \"%@\"", audioError3);
+    }
+    else {
+        [dieplayer setNumberOfLoops: -1];
+    }
+
+    self.inGameSounds  = [[NSDictionary alloc] initWithObjectsAndKeys:
+                          attack_1player,@"attack_1",
+                          attack_2player,@"attack_2",
+                          attack1player,@"attack1",
+                          attack2player,@"attack2",
+                          backgroundplayer,@"background",
+                          dieplayer,@"die",
+                          nil];
+}
+
+
 
 @end
